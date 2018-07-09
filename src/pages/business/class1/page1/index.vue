5dbd0afb559c63152a36d94ee787adb7e80be37b
孙昊翔 <673686754@qq.com>
2018-07-09 14:20:58 +0800

    element表格基本功能实现

--- src/pages/business/class1/page1/index.vue
+++ src/pages/business/class1/page1/index.vue
@@ -4,9 +4,11 @@
       功能 1 <d2-icon name="caret-right"/> 页面 1
     </h1>
     <d2-curd
-     style="width: 100%"
-     :columns="columns"
-     :data="data">
+      :columns="columns"
+      :data="data"
+      border
+      style="width: 100%"
+    >
     </d2-curd>
   </d2-container>
 </template>
@@ -20,36 +22,73 @@ export default {
           title: '日期',
           key: 'date',
           width: '100',
+          fixed: 'left',
         },
         {
           title: '姓名',
           key: 'name',
+          width: '300',
+        },
+        {
+          title: '省份',
+          key: 'province',
+          width: '300',
+        },
+        {
+          title: '市区',
+          key: 'city',
+          width: '300',
+        },
+        {
+          title: '邮编',
+          key: 'zip',
+          width: '300',
         },
         {
           title: '地址',
           key: 'address',
+          width: '220',
+          fixed: 'right',
         },
       ],
       data: [
         {
           date: '2016-05-02',
           name: '王小虎',
+          province: '上海',
+          city: '普陀区',
+          zip: 200333,
           address: '上海市普陀区金沙江路 1518 弄',
         },
         {
           date: '2016-05-04',
           name: '王小虎',
+          province: '上海',
+          city: '普陀区',
+          zip: 200333,
           address: '上海市普陀区金沙江路 1517 弄',
+          rowControl: {
+            rowClassName: 'warning-row',
+          },
         },
         {
           date: '2016-05-01',
           name: '王小虎',
+          province: '上海',
+          city: '普陀区',
+          zip: 200333,
           address: '上海市普陀区金沙江路 1519 弄',
         },
         {
           date: '2016-05-03',
           name: '王小虎',
+          province: '上海',
+          city: '普陀区',
+          zip: 200333,
           address: '上海市普陀区金沙江路 1516 弄',
+          rowControl: {
+            rowClassName: 'success-row',
+          },
         },
       ],
     };
@@ -57,3 +96,12 @@ export default {
 };
 </script>
 
+<style>
+  .el-table .warning-row {
+    background: oldlace;
+  }
+
+  .el-table .success-row {
+    background: #f0f9eb;
+  }
+</style>
--- src/plugin/d2-curd/d2-curd.vue
+++ src/plugin/d2-curd/d2-curd.vue
@@ -1,12 +1,18 @@
 <template>
   <div>
     <el-table
-      :data="data">
+      :data="data"
+      :stripe="stripe"
+      :border="border"
+      :height="height"
+      :max-height="maxHeight"
+      :row-class-name="tableRowClassName">
       <el-table-column
         v-for="(item, index) of columns"
         :prop="item.key"
         :label="item.title"
         :width="item.width ? item.width : ''"
+        :fixed="item.fixed ? item.fixed : false"
         :key="index">
       </el-table-column>
     </el-table>
@@ -31,10 +37,46 @@ export default {
       type: Array,
       required: true,
     },
+    /**
+     * @description 斑马纹
+     */
+    stripe: {
+      type: Boolean,
+      default: false,
+    },
+    /**
+     * @description 边框
+     */
+    border: {
+      type: Boolean,
+      default: false,
+    },
+    /**
+     * @description 表格高度
+     */
+    height: {
+      type: String,
+      default: 'auto',
+    },
+    maxHeight: {
+      type: String,
+      default: 'auto',
+    },
   },
   data() {
     return {
     };
+  },
+  methods: {
+    /**
+     * @description 表格状态
+     */
+    tableRowClassName({ row }) {
+      if (row.rowControl) {
+        return row.rowControl.rowClassName;
+      }
+      return '';
+    },
   },
 };
 </script>
