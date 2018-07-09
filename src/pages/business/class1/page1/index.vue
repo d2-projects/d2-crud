9b1c5dd763f90467dd8da3c0e749e5dd129ce7ff
孙昊翔 <673686754@qq.com>
2018-07-09 10:51:10 +0800

    表格基本布局

--- src/pages/business/class1/page1/index.vue
+++ src/pages/business/class1/page1/index.vue
@@ -3,6 +3,57 @@
     <h1 class="d2-mt-0">
       功能 1 <d2-icon name="caret-right"/> 页面 1
     </h1>
-    <d2-curd></d2-curd>
+    <d2-curd
+     style="width: 100%"
+     :columns="columns"
+     :data="data">
+    </d2-curd>
   </d2-container>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      columns: [
+        {
+          title: '日期',
+          key: 'date',
+          width: '100',
+        },
+        {
+          title: '姓名',
+          key: 'name',
+        },
+        {
+          title: '地址',
+          key: 'address',
+        },
+      ],
+      data: [
+        {
+          date: '2016-05-02',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄',
+        },
+        {
+          date: '2016-05-04',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1517 弄',
+        },
+        {
+          date: '2016-05-01',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1519 弄',
+        },
+        {
+          date: '2016-05-03',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1516 弄',
+        },
+      ],
+    };
+  },
+};
+</script>
+
--- src/plugin/d2-curd/d2-curd.vue
+++ src/plugin/d2-curd/d2-curd.vue
@@ -1,15 +1,39 @@
 <template>
   <div>
-    {{msg}}
+    <el-table
+      :data="data">
+      <el-table-column
+        v-for="(item, index) of columns"
+        :prop="item.key"
+        :label="item.title"
+        :width="item.width ? item.width : ''"
+        :key="index">
+      </el-table-column>
+    </el-table>
   </div>
 </template>
 
 <script>
 export default {
   name: 'd2Curd',
+  props: {
+    /**
+     * @description 表头数据
+     */
+    columns: {
+      type: Array,
+      required: true,
+    },
+    /**
+     * @description 表格数据
+     */
+    data: {
+      type: Array,
+      required: true,
+    },
+  },
   data() {
     return {
-      msg: 'd2-curd',
     };
   },
 };
