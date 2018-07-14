19a296f279d343021a96d5d3e032c1b1f35e3d4f
孙昊翔 <673686754@qq.com>
2018-07-14 15:38:56 +0800

    render组件

---
+++ src/plugin/d2-crud/components/renderComponent.vue
@@ -0,0 +1,29 @@
+<script>
+export default {
+  name: 'render-component',
+  props: {
+    /**
+     * @description 传入的render函数
+     */
+    renderFunc: {
+      type: Function,
+      required: true,
+    },
+    /**
+     * @description 传入的scope
+     */
+    scope: {
+      type: Object,
+      required: true,
+    },
+  },
+  render(h) {
+    return this.handleRender(h, this.scope);
+  },
+  methods: {
+    handleRender(h, scope) {
+      return this.renderFunc(h, scope);
+    },
+  },
+};
+</script>
