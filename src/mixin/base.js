a47b3cf4faef2f7b27d8dcc560352cf30eccb354
孙昊翔 <673686754@qq.com>
2018-08-29 17:00:14 +0800

    增加事件监听

--- src/mixin/base.js
+++ src/mixin/base.js
@@ -37,14 +37,80 @@ export default {
     /**
      * @description 行选中状态
      */
-    handleCurrentChange (val, oldVal) {
-      this.$emit('current-change', val, oldVal)
+    handleCurrentChange (currentRow, oldCurrentRow) {
+      this.$emit('current-change', currentRow, oldCurrentRow)
     },
     /**
-     * @description 复选框选中转状态
+     * @description 勾选数据时触发的事件
      */
-    handleSelectionChange (val) {
-      this.$emit('selection-change', val)
+    handleSelect (selection, row) {
+      this.$emit('select', selection, row)
+    },
+    /**
+     * @description 勾选全选时触发的事件
+     */
+    handleSelectAll (selection) {
+      this.$emit('select-all', selection)
+    },
+    /**
+     * @description 复选框选择项发生变化时触发的事件
+     */
+    handleSelectionChange (selection) {
+      this.$emit('selection-change', selection)
+    },
+    /**
+     * @description 单元格 hover 进入时触发的事件
+     */
+    handleCellMouseEnter (row, column, cell, event) {
+      this.$emit('cell-mouse-enter', row, column, cell, event)
+    },
+    /**
+     * @description 单元格 hover 退出时触发的事件
+     */
+    handleCellMouseLeave (row, column, cell, event) {
+      this.$emit('cell-mouse-leave', row, column, cell, event)
+    },
+    /**
+     * @description 单元格点击时触发的事件
+     */
+    handleCellClick (row, column, cell, event) {
+      this.$emit('cell-click', row, column, cell, event)
+    },
+    /**
+     * @description 单元格双击时触发的事件
+     */
+    handleCellDblclick (row, column, cell, event) {
+      this.$emit('cell-dblclick', row, column, cell, event)
+    },
+    /**
+     * @description 行点击时触发的事件
+     */
+    handleRowClick (row, event, column) {
+      this.$emit('row-click', row, event, column)
+    },
+    /**
+     * @description 行右键点击时触发的事件
+     */
+    handleRowContextmenu (row, event) {
+      this.$emit('row-contextmenu', row, event)
+    },
+    /**
+     * @description 行双击时触发的事件
+     */
+    handleRowDblclick (row, event) {
+      this.$emit('row-dblclick', row, event)
+    },
+    /**
+     * @description 表头点击时触发的事件
+     */
+    handleHeaderClick (column, event) {
+      this.$emit('header-click', column, event)
+    },
+    /**
+     * @description 表头右键点击时触发的事件
+     */
+    handleHeaderContextmenu (column, event) {
+      this.$emit('header-contextmenu', column, event)
     }
   }
 }
